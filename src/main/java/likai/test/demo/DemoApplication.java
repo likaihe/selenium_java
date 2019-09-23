19ff5076f2dec930fb0ef7c58b60fbaa0520489f
HeL <Likai.He@Vector.co.nz>
2019-09-23 16:54:30 +1200

    remove useless class

--- src/main/java/likai/test/demo/DemoApplication.java
+++ src/main/java/likai/test/demo/DemoApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		final TestRunner testRunner = new TestRunner();
 		SpringApplication.run(DemoApplication.class, args);
-		testRunner.start();
 
 	}
 
